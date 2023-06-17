7066ec5405e55aada3065b9ad645c611f2b6ef0e
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-06-17 19:06:42 -0400

    Added Multiple Die

--- DiceGame.kt
+++ DiceGame.kt
@@ -1,7 +1,10 @@
 package game
 
 fun main() {
-    val die = Die(Sides.TWENTY)
-    print(die.sideUp)
+    print("Creating a default Die\n")
+    val die1 = Die(Sides.TWENTY)
+    println(die1.sideUp)
+    val die2 =Die(Sides.SIX)
+    println(die2.sideUp)
     // die.sideUp = -100 // This code will not compile
 }
